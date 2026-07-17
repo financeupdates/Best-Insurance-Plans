4e40be9cb930bbe767ca3db5beeb8db54771e2be
financeupdates <nandininama57@gmail.com>
2026-07-17 15:08:18 +0530

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,1 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
 
+project = 'best insurance plans for my needs'
+author = 'best insurance plans for my needs'
+release = '1.0'
+
+extensions = [
+    'sphinx_sitemap',
+]
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "best insurance plans for my needs"
+
+# Sitemap
+html_baseurl = "https://fidelity-fidelity-netbenefits-log-in.readthedocs-hosted.com/"
+sitemap_url_scheme = "{link}"
